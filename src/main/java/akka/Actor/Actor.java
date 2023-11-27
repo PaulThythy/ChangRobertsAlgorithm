158499718a53e70ab0627c9ebf29a2eb6ff0a129
Paul THIRION <pt454976@MI103-02.iem>
2023-11-27 17:44:22 +0100

    started Actor.java

--- src/main/java/akka/Actor/Actor.java
+++ src/main/java/akka/Actor/Actor.java
@@ -1,5 +1,24 @@
 package akka.Actor;
 
-public class Actor {
-    
+import javax.swing.AbstractAction;
+import akka.actor.ActorRef;
+
+public class Actor extends AbstractActor {
+    private ActorRef acteur;
+
+    private Actor(){
+
+    }
+
+    // actor creation
+    public static Props props(){
+        return Props.create(Actor.class, );
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+            .match()
+            .build();
+    }
 }
--- src/main/java/akka/App.java
+++ src/main/java/akka/App.java
@@ -1,7 +1,14 @@
 package akka;
 
+import akka.actor.ActorSystem;
+
 public class App {
+    public static int NB_ACTORS = 10;
     public static void main(String[] args){
+        ActorSystem actorSystem = ActorSystem.create();
+
         
+
+        actorSystem.terminate();
     }
 }
