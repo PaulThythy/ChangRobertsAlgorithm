9205930c88c12f2afbb1e4945be6c959e9545676
PaulThythy <paul.thi@hotmail.fr>
2023-11-27 23:51:05 +0100

    work in progress

--- src/main/java/akka/Actor/Actor.java
+++ src/main/java/akka/Actor/Actor.java
@@ -2,17 +2,32 @@ package akka.Actor;
 
 import javax.swing.AbstractAction;
 import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.AbstractActor;
 
 public class Actor extends AbstractActor {
-    private ActorRef acteur;
+    private ActorRef next;
+    private int id;
+    private boolean participant = false;
+    private boolean leader = false;
 
-    private Actor(){
+    private Actor(int _id){
+        this.id = _id;
+        this.next = getContext().actorOf(Actor.props(++_id));
+    }
 
+    private Actor(int _id, ActorRef initiactor){
+        this.id = _id;
+        this.next = initiactor;
     }
 
     // actor creation
-    public static Props props(){
-        return Props.create(Actor.class, );
+    public static Props props(int _id){
+        return Props.create(Actor.class, _id);
+    }
+
+    public static Props props(int _id, ActorRef initiactor){
+        return Props.create(Actor.class, _id, initiactor);
     }
 
     @Override
--- src/main/java/akka/App.java
+++ src/main/java/akka/App.java
@@ -1,13 +1,17 @@
 package akka;
 
 import akka.actor.ActorSystem;
+import akka.actor.ActorRef;
+import akka.Actor.Actor;
 
 public class App {
+
     public static int NB_ACTORS = 10;
     public static void main(String[] args){
         ActorSystem actorSystem = ActorSystem.create();
 
-        
+        ActorRef initiactor = actorSystem.actorOf(Actor.props(0));
+        ActorRef lastActor = actorSystem.actorOf(Actor.props(NB_ACTORS-1, initiactor));
 
         actorSystem.terminate();
     }
