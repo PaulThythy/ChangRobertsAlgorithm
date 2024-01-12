2b4c6ea90908dd7d056d04afede5dab60d1dfcc3
Thendis <eddin.mkavavo@outlook.fr>
2024-01-12 18:31:46 +0100

    bite

--- src/main/java/perso/test/App.java
+++ src/main/java/perso/test/App.java
@@ -7,15 +7,15 @@ import perso.test.Actors.ActorClass;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
-        int nbActor = 25; //Limité par puissance de la machine
+        int nbActor = 5; //Limité par puissance de la machine
 
         ActorSystem acSystem = ActorSystem.create("MonSystem");
         ActorRef last = ActorClass.initCircle(acSystem, nbActor);
-        last.tell(new ActorClass.signal(-1, null), ActorRef.noSender());
-
+        last.tell(new ActorClass.signal(-1, null), ActorRef.noSender()); //Envoi du message de bouclage
         
+        //Thread.sleep(2000);
         //acSystem.terminate();
     }
 }
